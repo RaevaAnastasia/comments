45cb6c9898961ee1fab02c4785c6be269fdb2d1d
Anastasia Raeva <anastassia.raeva@gmail.com>
2020-01-29 15:53:25 +0300

    Updates

---
+++ precache-manifest.0452fe8af2abc70b63841d1da68363bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80436fe92ef563e8528c7058f303b8ca",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "4d2b7c0f5c0e39917fc1",
+    "url": "/react-gh-pages/static/css/main.28bc429a.chunk.css"
+  },
+  {
+    "revision": "785b57c15e14d70ad95f",
+    "url": "/react-gh-pages/static/js/2.6e7693b2.chunk.js"
+  },
+  {
+    "revision": "4d2b7c0f5c0e39917fc1",
+    "url": "/react-gh-pages/static/js/main.dcd71aca.chunk.js"
+  },
+  {
+    "revision": "1c915bef088c1c670371",
+    "url": "/react-gh-pages/static/js/runtime-main.fdf4b00b.js"
+  }
+]);
