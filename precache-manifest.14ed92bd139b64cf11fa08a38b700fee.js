692cb33666b948002009bfbe39e09e9ef5ed3e3c
Anastasia Raeva <anastassia.raeva@gmail.com>
2020-01-29 15:54:23 +0300

    Updates

---
+++ precache-manifest.14ed92bd139b64cf11fa08a38b700fee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d3f11c48de3bea5c41b2ae9e472e755",
+    "url": "/comments/index.html"
+  },
+  {
+    "revision": "61f70d38b480c4c9ddb7",
+    "url": "/comments/static/css/main.28bc429a.chunk.css"
+  },
+  {
+    "revision": "785b57c15e14d70ad95f",
+    "url": "/comments/static/js/2.6e7693b2.chunk.js"
+  },
+  {
+    "revision": "61f70d38b480c4c9ddb7",
+    "url": "/comments/static/js/main.05af9d1a.chunk.js"
+  },
+  {
+    "revision": "e84799345345d8fb6c37",
+    "url": "/comments/static/js/runtime-main.66096a48.js"
+  }
+]);
