aa54d4ac638236ba23e17ddd78affd225f90ab2b
Anastasia Raeva <anastassia.raeva@gmail.com>
2020-01-29 16:12:36 +0300

    Edit code style

--- src/News/News.js
+++ src/News/News.js
@@ -5,19 +5,19 @@ import './News.css';
 function News(props) {
     let data = props.data;
     let newsList = data.map(function(elem, index) {
-            return ( < Article key = { index } data = { elem }/>);
-            });
+        return ( < Article key = { index } data = { elem }/>);
+    });
 
     let noNews = function() {
         return ( <div className = "news__no" >
-            <p > К сожалению, новостей нет </p>  
+                <p > К сожалению, новостей нет </p>  
             </div>
         );
     }
 
     return ( <div className = "news"> 
-        { data.length === 0 ? noNews() : newsList }
-        <strong> Всего комментариев: { data.length } </strong>  
+            { data.length === 0 ? noNews() : newsList }
+            <strong> Всего комментариев: { data.length } </strong>  
         </div>
     );
 }
